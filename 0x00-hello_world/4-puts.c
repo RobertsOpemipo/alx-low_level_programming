b98a4f19b534974a4e1d13ac219b5924134d04d1
RobertsOpemipo <opemipo.roberts@yahoo.co.uk>
2022-09-10 05:06:02 -0700

    Task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -4,11 +4,11 @@
 
 /**
  *
- *  * main - Entry point
+ *  main - Entry point
+ *  Programming is like building a multilingual puzzle
+ * 
  *
- *   *
- *
- *    * Return: Always 0 (Success)
+ * Return: Always 0 (Success)
  *
  **/
 
