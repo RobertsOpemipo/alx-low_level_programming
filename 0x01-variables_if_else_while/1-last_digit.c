e9ffe46a32e462ac1e74b02ed3e46d4d688e55c0
RobertsOpemipo <opemipo.roberts@yahoo.co.uk>
2022-09-10 10:14:57 -0700

    Task 1

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -3,9 +3,9 @@
 #include <time.h>
 
 /** 
- * main - Entry point
- * Return: Always 0 (Success)
- */
+* main - Entry point
+* Return: Always 0 (Success)
+**/
 
 int main(void)
 
@@ -23,6 +23,6 @@ if (L > 5)
 else if (L == 0)
 	printf("%s %d is %d and is 0\n", str, n, L);
 else if (L < 6)
-	printf("%s %d is %d and is less than 6 and not 0\n", str,n, L);
+	printf("%s %d is %d and is less than 6 and not 0\n", str, n, L);
 return (0);
 }
