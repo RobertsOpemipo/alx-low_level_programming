de893cb76a8d7a13e29a6eae74e9bdf1a9d821c4
RobertsOpemipo <opemipo.roberts@yahoo.co.uk>
2022-09-13 07:06:54 -0700

    Task 0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,9 +1,9 @@
-#include <main.h>
+#include "main.h"
 
 /**
  * main - print putchar
- * Return: always 0
- */
+ * Return 0
+**/
 
 int main(void)
 {
@@ -15,7 +15,7 @@ int main(void)
 	_putchar("h");
 	_putchar("a");
 	_putchar("r");
-	_putchar("/n");
+	_putchar("\n");
 
 	return (0);
 }
