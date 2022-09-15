34df3b8ec9e121038eeccfdc6d8c5589ab0f70e5
RobertsOpemipo <opemipo.roberts@yahoo.co.uk>
2022-09-15 15:05:51 -0700

    Task 7

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,28 @@
+include "main.h"
+
+/**
+ * print_diagonal - print slash
+ * @n: params
+ */
+
+void print_diagonal(int n)
+
+{
+
+int i = 0;
+int j;
+
+if (n > 0)
+{
+	for (; i < n; i++)
+	{
+		_putchar(' ');
+	}
+	_putchar(92);
+	_putchar('\n');
+}
+else
+{
+	_putchar('\n');
+}
+}
